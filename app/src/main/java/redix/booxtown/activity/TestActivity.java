ba09074c2080961cb3211e9c2f0c298c66038648
dodangduong <dodangduong.it.1992@gmail.com>
2016-08-26 23:17:02 +0700

    comentn

---
+++ app/src/main/java/redix/booxtown/activity/TestActivity.java
@@ -0,0 +1,20 @@
+package redix.booxtown.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import redix.booxtown.R;
+
+/**
+ * Created by thuyetpham94 on 26/08/2016.
+ */
+public class TestActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.customtabar);
+
+    }
+}
---
+++ app/src/main/java/redix/booxtown/custom/MenuTopCustom.java
@@ -0,0 +1,7 @@
+package redix.booxtown.custom;
+
+/**
+ * Created by thuyetpham94 on 26/08/2016.
+ */
+public class MenuTopCustom {
+}
