55989a977998620fff8bccec6d4c23b0b155d707
dodangduong <dodangduong.it.1992@gmail.com>
2016-08-26 21:04:00 +0700

    comentn

--- app/src/main/java/redix/booxtown/Activity/Setting.java
+++ app/src/main/java/redix/booxtown/Activity/Setting.java
@@ -1,15 +1,275 @@
 package redix.booxtown.activity;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.app.Fragment;
+import android.app.TimePickerDialog;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Calendar;
 
 import redix.booxtown.R;
 
-public class Setting extends AppCompatActivity {
+public class Setting extends AppCompatActivity implements LocationListener,OnMapReadyCallback,GoogleMap.OnMapLongClickListener, GoogleMap.OnInfoWindowClickListener {
+
+    TextView besttime1, besttime2;
+    private GoogleMap mMap;
+    private SupportMapFragment mMapFragment;
+    Marker mMarker;
+    private boolean flag = false;
+    Geocoder geocoder;
+    TextView txt_setting_besttime;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        txt_setting_besttime= (TextView)findViewById(R.id.txt_setting_besttime);
+        ImageView imv_setting_pass = (ImageView)findViewById(R.id.imv_setting_editpass);
+        imv_setting_pass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Dialog dialog = new Dialog(Setting.this);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.dialog_custom_editpass);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+
+                ImageView imageView = (ImageView) dialog.findViewById(R.id.imv_close_dialog_changepass);
+                imageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+
+        Switch switch_setting_editpass = (Switch) findViewById(R.id.switch_setting_besttime);
+        final Dialog dialog = new Dialog(Setting.this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_custom_seting_besttime);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        switch_setting_editpass.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+                if (b == false) {
+                    dialog.dismiss();
+                } else {
+
+                    dialog.show();
+
+                    //chinh thơi gian
+
+                    besttime1 = (TextView) dialog.findViewById(R.id.txt_seting_besttime1);
+                    besttime2 = (TextView) dialog.findViewById(R.id.txt_seting_besttime2);
+
+                    besttime1.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Calendar mcurrentTime = Calendar.getInstance();
+                            int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                            int minute = mcurrentTime.get(Calendar.MINUTE);
+                            TimePickerDialog mTimePicker;
+                            mTimePicker = new TimePickerDialog(Setting.this, new TimePickerDialog.OnTimeSetListener() {
+                                @Override
+                                public void onTimeSet(TimePicker timePicker, int i, int i1) {
+
+                                    besttime1.setText(convertTime(i) + ":" + convertTime(i1) + " " + showTime(i, i1));
+                                }
+                            }, hour, minute, true);
+                            mTimePicker.setTitle("");
+                            mTimePicker.show();
+                        }
+                    });
+
+                    besttime2.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Calendar mcurrentTime = Calendar.getInstance();
+                            int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                            int minute = mcurrentTime.get(Calendar.MINUTE);
+                            TimePickerDialog mTimePicker;
+                            mTimePicker = new TimePickerDialog(Setting.this, new TimePickerDialog.OnTimeSetListener() {
+                                @Override
+                                public void onTimeSet(TimePicker timePicker, int i, int i1) {
+
+                                    besttime2.setText(convertTime(i) + ":" + convertTime(i1) + " " + showTime(i, i1));
+                                }
+                            }, hour, minute, true);
+                            mTimePicker.setTitle("");
+                            mTimePicker.show();
+                        }
+                    });
+
+                    ImageView imageView = (ImageView) dialog.findViewById(R.id.imv_close_dialog_submit);
+                    imageView.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            dialog.dismiss();
+                        }
+                    });
+
+                    Button btn_setingdialog_time =(Button)dialog.findViewById(R.id.btn_setingdialog_time);
+                    btn_setingdialog_time.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            txt_setting_besttime.setText(besttime1.getText() +"  -  "+ besttime2.getText());
+                            dialog.dismiss();
+                        }
+                    });
+                }
+            }
+        });
+
+        Switch switch_location = (Switch) findViewById(R.id.switch_seting_location);
+        mMapFragment = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment));
+        mMapFragment.getMapAsync(Setting.this);
+        switch_location.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b == false) {
+                    getSupportFragmentManager().beginTransaction().hide(mMapFragment).commit();
+                } else {
+                    getSupportFragmentManager().beginTransaction().show(mMapFragment).commit();
+                }
+            }
+        });
+
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    public String showTime(int hour, int min) {
+        String format;
+        if (hour == 0) {
+            hour += 12;
+            format = "AM";
+        } else if (hour == 12) {
+            format = "PM";
+        } else if (hour > 12) {
+            hour -= 12;
+            format = "PM";
+        } else {
+            format = "AM";
+        }
+        return format;
+    }
+
+    public String convertTime(int hour) {
+        if (hour < 10) {
+            return "0" + hour;
+        } else {
+            return hour + "";
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        if (Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission(Setting.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(Setting.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String provider = service.getBestProvider(criteria, false);
+        // Add a marker in Sydney and move the camera
+        Location location = service.getLastKnownLocation(provider);
+        // create marker
+        MarkerOptions marker = new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())).title("Hello Maps");
+
+        // Changing marker icon
+        marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_sell));
+
+        // adding marker
+        mMap.addMarker(marker);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 8));
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+        mMap.getUiSettings().setAllGesturesEnabled(true);
+
+        mMap.setTrafficEnabled(true);
     }
 }
