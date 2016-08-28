18def3a3b16c4aea87b8fd0ef60ddca9b8e3d9f7
dodangduong <dodangduong.it.1992@gmail.com>
2016-08-29 01:49:48 +0700

    comentn

--- app/src/main/java/redix/booxtown/activity/InteractThreadActivity.java
+++ app/src/main/java/redix/booxtown/activity/InteractThreadActivity.java
@@ -1,6 +1,9 @@
 package redix.booxtown.activity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
