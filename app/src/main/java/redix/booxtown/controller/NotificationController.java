39ee6608ec0c7238872628a42c36377609c6502a
Gandalf0303 <tung.ars150293@gmail.com>
2016-09-27 23:46:20 +0700

    Update NotificationController.java

--- app/src/main/java/redix/booxtown/controller/NotificationController.java
+++ app/src/main/java/redix/booxtown/controller/NotificationController.java
@@ -28,7 +28,7 @@ public class NotificationController {
         this.mActivity = mActivity;
     }
 
-    public boolean sendNotification(List<Notification> notifications){
+     public boolean sendNotification(List<Hashtable> notifications){
         Hashtable obj = new Hashtable();
         obj.put("notification_list",notifications);
         try {
@@ -47,5 +47,66 @@ public class NotificationController {
             String s = ex.getMessage();
         }
         return false;
+    }
+
+    public List<Notification> getALllNotificationTop(String session_id, int top, int from){
+        Call<NotificationResult> getTopNotification = service.getTopNotification(session_id, top, from);
+        try {
+            if (android.os.Build.VERSION.SDK_INT > 9) {
+                StrictMode.ThreadPolicy policy =
+                        new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                StrictMode.setThreadPolicy(policy);
+            }
+            NotificationResult str = getTopNotification.execute().body();
+            if (str.getCode()==200){
+                return str.getNotifi();
+            }
+        } catch (Exception ex) {
+        }
+        return null;
+    }
+
+    public Boolean changeStatusNotification(String session_id, int thread_id){
+        Hashtable obj= new Hashtable();
+        obj.put("session_id",session_id);
+        obj.put("notifi_id",thread_id);
+        Call<Result> changeStatusNotifi = service.changeStatusNotification(obj);
+        try {
+            if (android.os.Build.VERSION.SDK_INT > 9) {
+                StrictMode.ThreadPolicy policy =
+                        new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                StrictMode.setThreadPolicy(policy);
+            }
+            Result str = changeStatusNotifi.execute().body();
+            if (str.getCode() == 200){
+                success = true;
+            }
+        } catch (Exception ex) {
+            success = false;
+        }
+        return success;
+    }
+
+    public Boolean changeStatusUnreadNotification(String session_id, int thread_id){
+        Hashtable obj= new Hashtable();
+        obj.put("session_id",session_id);
+        obj.put("topic_id",thread_id);
+
+
+        Call<Result> changeStatusNotifi = service.changeStatusUnreadNotification(obj);
+        try {
+            if (android.os.Build.VERSION.SDK_INT > 9) {
+                StrictMode.ThreadPolicy policy =
+                        new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                StrictMode.setThreadPolicy(policy);
+            }
+            Result str = changeStatusNotifi.execute().body();
+            if (str.getCode() == 200){
+                success = true;
+            }
+        } catch (Exception ex) {
+            success = false;
+        }
+        return success;
     }
 }
