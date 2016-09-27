0ac4c671a1d1023a3893bed3d7a5db10aae15854
duong <dodangduong.it.1992@gmail.com>
2016-09-27 17:30:00 +0700

    phase2

--- app/src/main/java/redix/booxtown/Controller/UserController.java
+++ app/src/main/java/redix/booxtown/Controller/UserController.java
@@ -128,36 +128,29 @@ public class UserController {
         return null;
     }
 
-<<<<<<< HEAD
-    public boolean changePassword(String session_id,String pwd_old,String pwd_new){
-        Call<Result> profile = service.changepassword(session_id,pwd_old,pwd_new);
-=======
-    public String getUserID(String session_id){
-        Call<Result> profile = service.getuserID(session_id);
->>>>>>> origin/master
-        try {
-            if (android.os.Build.VERSION.SDK_INT > 9) {
-                StrictMode.ThreadPolicy policy =
-                        new StrictMode.ThreadPolicy.Builder().permitAll().build();
-                StrictMode.setThreadPolicy(policy);
-            }
-            Result str = profile.execute().body();
-            if (str.getCode() == 200){
-<<<<<<< HEAD
-                return true;
-            }
-        } catch (Exception ex) {
-        }
-        return false;
-    }
-=======
-                return str.getSession_id();
-            }
-        } catch (Exception ex) {
-        }
-        return null;
-    }
-
->>>>>>> origin/master
+//    public boolean changePassword(String session_id,String pwd_old,String pwd_new){
+//        Call<Result> profile = service.changepassword(session_id,pwd_old,pwd_new);
+//    public String getUserID(String session_id){
+//        Call<Result> profile = service.getuserID(session_id);
+//        try {
+//            if (android.os.Build.VERSION.SDK_INT > 9) {
+//                StrictMode.ThreadPolicy policy =
+//                        new StrictMode.ThreadPolicy.Builder().permitAll().build();
+//                StrictMode.setThreadPolicy(policy);
+//            }
+//            Result str = profile.execute().body();
+//            if (str.getCode() == 200){
+//                return true;
+//            }
+//        } catch (Exception ex) {
+//        }
+//        return false;
+//    }
+//                return str.getSession_id();
+//            }
+//        } catch (Exception ex) {
+//        }
+//        return null;
+//    }
 
 }
